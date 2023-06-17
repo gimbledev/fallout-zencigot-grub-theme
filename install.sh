9831748c082018e9fc0fe44c42d33d703bff75f2
Gimble <79212283+gimbledev@users.noreply.github.com>
2023-06-17 15:28:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-GRUB_THEME='fallout-grub-theme'
+GRUB_THEME='fallout-zencigot-grub-theme'
 INSTALLER_LANG='English'
 
 # Check dependencies
@@ -63,7 +63,7 @@ select l in "${INSTALLER_LANG_NAMES[@]}"; do
 done < /dev/tty
 
 echo 'Fetching and unpacking theme'
-wget -O - https://github.com/shvchk/${GRUB_THEME}/archive/master.tar.gz | tar -xzf - --strip-components=1
+wget -O - https://github.com/gimbledev/${GRUB_THEME}/archive/master.tar.gz | tar -xzf - --strip-components=1
 
 if [[ "$INSTALLER_LANG" != "English" ]]; then
     echo "Changing language to ${INSTALLER_LANG}"
